d1423baf83e6479f4acddda131b7f63324d17007
springbootlearning <41922166+springbootlearning@users.noreply.github.com>
2018-11-12 20:52:08 +0100

    Add files via upload
    Custom banner creation in spring boot

---
+++ src/test/java/com/ikea/spring/custom/banner/SpringBootCustomBannerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ikea.spring.custom.banner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootCustomBannerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
